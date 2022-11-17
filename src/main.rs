00aa785728decd32b398e6c5587acd00af57fdb3
Karahan Buhan <root@karahanbuhan.com>
2022-11-17 23:13:51 +0300

    Add simple player movement

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+use bevy::prelude::*;
+
+use crate::player::PlayerPlugin;
+
+mod player;
+
+pub const TILE_SIZE: f32 = 1.0;
+
 fn main() {
-    println!("Pong!");
+    App::new()
+        .insert_resource(ClearColor(Color::BLACK))
+        .add_plugins(DefaultPlugins.set(WindowPlugin {
+            window: WindowDescriptor {
+                width: 800.0,
+                height: 600.0,
+                title: String::from("Pong"),
+                resizable: false,
+                ..default()
+            },
+            ..default()
+        }))
+        .add_plugin(PlayerPlugin)
+        .add_startup_system(spawn_camera)
+        .run();
+}
+
+fn spawn_camera(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }
---
+++ src/player.rs
@@ -0,0 +1,44 @@
+use bevy::prelude::*;
+
+use crate::TILE_SIZE;
+
+pub struct PlayerPlugin;
+
+#[derive(Component)]
+pub struct Player {}
+
+impl Plugin for PlayerPlugin {
+    fn build(&self, app: &mut App) {
+        app.add_startup_system(spawn_player)
+            .add_system(player_movement);
+    }
+}
+
+fn player_movement(
+    mut player_query: Query<(&Player, &mut Transform)>,
+    keyboard: Res<Input<KeyCode>>,
+    time: Res<Time>,
+) {
+    let (_player, mut transform) = player_query.single_mut();
+
+    if keyboard.pressed(KeyCode::W) && transform.translation.y < 250.0 {
+        transform.translation.y += 300.0 * time.delta_seconds() * TILE_SIZE;
+    }
+    if keyboard.pressed(KeyCode::S) && transform.translation.y > -250.0 {
+        transform.translation.y -= 300.0 * time.delta_seconds() * TILE_SIZE;
+    }
+}
+
+fn spawn_player(mut commands: Commands) {
+    commands
+        .spawn(SpriteBundle {
+            sprite: Sprite {
+                color: Color::rgb(1.0, 1.0, 1.0),
+                custom_size: Some(Vec2::new(10.0 * TILE_SIZE, 100.0 * TILE_SIZE)),
+                ..default()
+            },
+            ..default()
+        })
+        .insert(Name::new("Player"))
+        .insert(Player {});
+}
