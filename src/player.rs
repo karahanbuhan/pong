a17727a4d2a5cd29b3ce93540db30c27bc5ba0df
Karahan Buhan <root@karahanbuhan.com>
2022-11-21 18:50:49 +0300

    Add second player and controls

--- src/player.rs
+++ src/player.rs
@@ -3,33 +3,72 @@ use bevy::prelude::*;
 use crate::TILE_SIZE;
 
 #[derive(Component)]
-pub struct Player {}
+pub struct Player(Location);
 
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_startup_system(spawn_player)
+        app.add_startup_system(spawn_players)
             .add_system(player_movement);
     }
 }
+
+pub enum Location {
+    Left,
+    Right,
+}
+
+impl Location {
+    pub fn to_vec(&self) -> Vec3 {
+        match *self {
+            Location::Left => Vec3::new(-350.0, 0.0, 0.0),
+            Location::Right => Vec3::new(350.0, 0.0, 0.0),
+        }
+    }
+}
+
+const MOVE_SPEED: f32 = 400.0;
 
 fn player_movement(
     mut player_query: Query<(&Player, &mut Transform)>,
     keyboard: Res<Input<KeyCode>>,
     time: Res<Time>,
 ) {
-    let (_player, mut transform) = player_query.single_mut();
+    for (Player(location), mut transform) in player_query.iter_mut() {
+        let mut move_vertical = |mul: f32| {
+            transform.translation.y += mul * time.delta_seconds() * MOVE_SPEED * TILE_SIZE;
+        };
 
-    if keyboard.pressed(KeyCode::W) && transform.translation.y < 250.0 {
-        transform.translation.y += 300.0 * time.delta_seconds() * TILE_SIZE;
-    }
-    if keyboard.pressed(KeyCode::S) && transform.translation.y > -250.0 {
-        transform.translation.y -= 300.0 * time.delta_seconds() * TILE_SIZE;
+        match location {
+            Location::Left => {
+                if keyboard.pressed(KeyCode::W) {
+                    move_vertical(1.0);
+                }
+                if keyboard.pressed(KeyCode::S) {
+                    move_vertical(-1.0);
+                }
+            }
+            Location::Right => {
+                if keyboard.pressed(KeyCode::Up) {
+                    move_vertical(1.0);
+                }
+                if keyboard.pressed(KeyCode::Down) {
+                    move_vertical(-1.0);
+                }
+            }
+        }
+
+        transform.translation.y = transform.translation.y.clamp(-250.0, 250.0)
     }
 }
 
-fn spawn_player(mut commands: Commands) {
+fn spawn_players(mut commands: Commands) {
+    spawn_player(&mut commands, Location::Left);
+    spawn_player(&mut commands, Location::Right);
+}
+
+fn spawn_player(commands: &mut Commands, location: Location) {
     commands
         .spawn(SpriteBundle {
             sprite: Sprite {
@@ -40,10 +79,9 @@ fn spawn_player(mut commands: Commands) {
             ..default()
         })
         .insert(SpatialBundle {
-            transform: Transform::from_translation(Vec3::new(-350.0, 0.0, 0.0)),
+            transform: Transform::from_translation(location.to_vec()),
             visibility: Visibility { is_visible: true },
             ..default()
         })
-        .insert(Name::new("Player"))
-        .insert(Player {});
+        .insert(Player(location));
 }
